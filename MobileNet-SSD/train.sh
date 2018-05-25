0647b6f47e02984dcb50847368e0eddff58db96a
GuoJaw <39326730+GuoJaw@users.noreply.github.com>
2018-05-25 20:11:40 +0800

    Update train.sh

--- MobileNet-SSD/train.sh
+++ MobileNet-SSD/train.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
-if ! test -f example/MobileNetSSD_train.prototxt ;then
-	echo "error: example/MobileNetSSD_train.prototxt does not exist."
-	echo "please use the gen_model.sh to generate your own model."
-        exit 1
-fi
+
 mkdir -p snapshot
-../../build/tools/caffe train -solver="solver_train.prototxt" \  #已经改好
+../build/tools/caffe train -solver="solver_train.prototxt" \  #已经改好
 -weights="MobileNet_Pre_Train.caffemodel" \ #已经改好
 -gpu 0 
